deb9c85d19c2cfd6f7b59aa8e7658c992296f9e6
Geo <george.nogovitsin@gmail.com>
2023-02-26 23:56:23 +0300

    snak

--- snake.py
+++ snake.py
@@ -2,15 +2,19 @@ import pygame
 from sys import exit
 from random import randint
 
+pygame.init()
 
-FPS = 5
+FPS = 150000000
 WIDTH, HEIGHT = 400, 400
-clock = pygame.time.Clock()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
 
 snake_surf = pygame.Surface((WIDTH/10, HEIGHT/10))
 snake_surf.fill('Yellow')
 snake_rect = snake_surf.get_rect(bottomright = (80,80))
+snake_tail1 = pygame.Surface((WIDTH/10, HEIGHT/10))
+snake_tail1.fill('Green')
+snake_tail1_rect = snake_tail1.get_rect(bottomright = (snake_rect.x-40, snake_rect.y-40))
 
 direction = 9
 while True:
@@ -24,6 +28,15 @@ while True:
             elif event.key == pygame.K_w: direction = 2
             elif event.key == pygame.K_s: direction = 3
 
+    if snake_rect.x >= WIDTH:
+        snake_rect.x = 0
+    elif snake_rect.x < 0:
+        snake_rect.x = WIDTH
+    if snake_rect.y >= HEIGHT:
+        snake_rect.y = 0
+    elif snake_rect.y < 0:
+        snake_rect.y = HEIGHT
+
     if direction == 9:
         pass
     elif direction == 0:
@@ -35,20 +48,13 @@ while True:
     elif direction == 3:
         snake_rect.y += 40
 
-    if snake_rect.x > WIDTH:
-        snake_rect.x = 0
-    elif snake_rect.x < 0:
-        snake_rect.x = WIDTH
-    if snake_rect.y > HEIGHT:
-        snake_rect.y = 0
-    elif snake_rect.y < 0:
-        snake_rect.y = HEIGHT
+
 
 
 
     screen.fill('Lightblue')
     screen.blit(snake_surf, snake_rect)
-
+    screen.blit(snake_tail1, snake_tail1_rect)
 
 
 
