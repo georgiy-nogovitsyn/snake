d7b68ce25c20370bd08260c2e2e1f5f9e8291ec4
Geo <george.nogovitsin@gmail.com>
2023-02-28 21:40:01 +0300

    commit

--- snake.py
+++ snake.py
@@ -1,63 +1,31 @@
 import pygame
+from pygame.math import Vector2
 from sys import exit
 from random import randint
 
+class Fruit:
+    def __init__(self):
+        self.fruit_position = Vector2(randint(1, CELL_NUMBER), randint(1, CELL_NUMBER))
+
+    def draw_fruit(self):
+        self.fruit_rect = pygame.Rect(self.fruit_position, CELL_SIZE, CELL_SIZE, pygame.Color('Green'))
+
 pygame.init()
 
-FPS = 150000000
-WIDTH, HEIGHT = 400, 400
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+FPS = 60
+CELL_SIZE = 30
+CELL_NUMBER = 20
+screen = pygame.display.set_mode((CELL_SIZE * CELL_NUMBER, CELL_SIZE * CELL_NUMBER))
 clock = pygame.time.Clock()
-
-snake_surf = pygame.Surface((WIDTH/10, HEIGHT/10))
-snake_surf.fill('Yellow')
-snake_rect = snake_surf.get_rect(bottomright = (80,80))
-snake_tail1 = pygame.Surface((WIDTH/10, HEIGHT/10))
-snake_tail1.fill('Green')
-snake_tail1_rect = snake_tail1.get_rect(bottomright = (snake_rect.x-40, snake_rect.y-40))
-
-direction = 9
+fruit = Fruit()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_d: direction = 0
-            elif event.key == pygame.K_a: direction = 1
-            elif event.key == pygame.K_w: direction = 2
-            elif event.key == pygame.K_s: direction = 3
-
-    if snake_rect.x >= WIDTH:
-        snake_rect.x = 0
-    elif snake_rect.x < 0:
-        snake_rect.x = WIDTH
-    if snake_rect.y >= HEIGHT:
-        snake_rect.y = 0
-    elif snake_rect.y < 0:
-        snake_rect.y = HEIGHT
-
-    if direction == 9:
-        pass
-    elif direction == 0:
-        snake_rect.x += 40
-    elif direction == 1:
-        snake_rect.x -= 40
-    elif direction == 2:
-        snake_rect.y -= 40
-    elif direction == 3:
-        snake_rect.y += 40
-
-
-
-
-
     screen.fill('Lightblue')
-    screen.blit(snake_surf, snake_rect)
-    screen.blit(snake_tail1, snake_tail1_rect)
 
-
-
+    screen.blit(fruit.)
 
 
     pygame.display.update()
